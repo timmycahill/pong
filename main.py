fb7ae8b1b6d760ce4916ff92eb29cad3fe3e4bd6
Tim Cahill <timcahill@Tims-Air.lan>
2022-04-07 20:26:58 -0400

    Initial commit. Basic layout and player paddle movement implemented.

---
+++ main.py
@@ -0,0 +1,8 @@
+import pong
+
+def main():
+    game = pong.Game()
+    game.run()
+
+if __name__ == '__main__':
+    main()
---
+++ pong.py
@@ -0,0 +1,81 @@
+import pygame, os
+
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 600
+FPS = 60
+THICKNESS = 15
+PADDLE_LENGTH = 100
+PADDLE_SPEED = 5
+
+class Position():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Game():
+    def __init__(self):
+        # Initialze window
+        pygame.init()
+        self.WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("Pong")
+
+        # Initialize variables
+        self.paddleLeftPos = Position(5, (WINDOW_HEIGHT - PADDLE_LENGTH) / 2)
+        self.playerDirection = 0
+        self.paddleRightPos = Position(WINDOW_WIDTH - THICKNESS - 5, (WINDOW_HEIGHT - PADDLE_LENGTH) / 2)
+        self.computerDirection = 0
+
+
+    def _processInputs(self):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.playerDirection -= PADDLE_SPEED
+                if event.key == pygame.K_DOWN:
+                    self.playerDirection += PADDLE_SPEED
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    self.playerDirection += PADDLE_SPEED
+                if event.key == pygame.K_DOWN:
+                    self.playerDirection -= PADDLE_SPEED
+
+            if event.type == pygame.QUIT:
+                self.isRunning = False
+
+    def _updateGame(self):
+        self.paddleLeftPos.y += self.playerDirection
+
+
+    def _generateOutputs(self):
+        # Clear screen
+        self.WINDOW.fill("black")
+
+        # Draw walls
+        topWall = pygame.Rect(0, 0, WINDOW_WIDTH, THICKNESS)
+        self.WINDOW.fill("white", rect=topWall)
+        bottomWall = pygame.Rect(0, WINDOW_HEIGHT - THICKNESS, WINDOW_WIDTH, THICKNESS)
+        self.WINDOW.fill("white", rect=bottomWall)
+
+        # Draw paddles
+        leftPaddle = pygame.Rect(self.paddleLeftPos.x, self.paddleLeftPos.y, THICKNESS, PADDLE_LENGTH)
+        self.WINDOW.fill("white", rect=leftPaddle)
+        rightPaddle = pygame.Rect(self.paddleRightPos.x, self.paddleRightPos.y, THICKNESS, PADDLE_LENGTH)
+        self.WINDOW.fill("white", rect=rightPaddle)
+
+
+        # Update screen
+        pygame.display.flip()
+
+    def run(self):
+        # Game loop
+        clock = pygame.time.Clock()
+        self.isRunning = True
+        while self.isRunning:
+            clock.tick(FPS)
+            self._processInputs()
+            self._updateGame()
+            self._generateOutputs()
+
+
+        # Uninitialize everything and close application
+        pygame.quit()
